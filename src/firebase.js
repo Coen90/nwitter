5b53bf864e107fc616e9407ffc9bcc574d3feee5
Hyuntae <bht9011@gmail.com>
2021-12-07 15:18:17 +0900

    commit for laptop

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAPzmaepM7WD6xQnOq-Uy4RbvPKVna49ck",
+  authDomain: "nwitter-66870.firebaseapp.com",
+  projectId: "nwitter-66870",
+  storageBucket: "nwitter-66870.appspot.com",
+  messagingSenderId: "1055727734604",
+  appId: "1:1055727734604:web:85d1923b584a6723a5c9e9",
+};
+
+export default initializeApp(firebaseConfig);
